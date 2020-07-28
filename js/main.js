8ac3616fa2441a4cdc3c3f3fb4684bc05793a642
YEHEUM\qkswk <cyheum@naver.com>
2020-07-29 01:36:20 +0900

    복습 및 자바스크립트 수정

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const commentText = document.getElementsByClassName("commentText");
 const commentButton1 = document.getElementsByClassName("commentButton");
 const comment = document.getElementsByTagName("ul");
 const name = document.getElementById("yourName");
-let isItColor = true;
+let isItColor = [];
 let isHeartPush = true;
 
 commentButton1[0].addEventListener("click", function () {
@@ -16,25 +16,48 @@ commentButton1[0].addEventListener("click", function () {
 
   wrapCommentA.append(name.text);
   wrapCommentLi.append(wrapCommentA);
-  wrapCommentLi.append(commentText[0].value);
+  wrapCommentLi.appenrd(commentText[0].value);
   wrapCommentLi.append(wrapCommentImg);
 
   comment[0].append(wrapCommentLi);
   commentText[0].value = null;
+
   event.preventDefault();
 });
 
 let heartBtn = document.getElementsByClassName("colorHeart");
 
-heartBtn[0].addEventListener("click", function () {
-  if (!isItColor) {
-    heartBtn[0].src = "img/redheart.png";
-    isItColor = true;
-  } else {
-    heartBtn[0].src = "img/heart.png";
-    isItColor = false;
-  }
-});
+for (let i = 0; i < heartBtn.length; i++) {
+  isItColor[i] = false;
+  let value = heartBtn[i];
+
+  value.addEventListener("click", function () {
+    let isBigHeart = this.classList.contains("bigHeart");
+
+    if (isBigHeart) {
+      let article = this.parentNode.parentNode.parentNode;
+      console.log(article);
+      let peopleCount = article.getElementsByClassName("peopleCount")[0];
+      console.log(peopleCount);
+
+      let newPeopleCount;
+      if (!isItColor[i]) {
+        newPeopleCount = "외 1,399명";
+      } else {
+        newPeopleCount = "외 1,398명";
+      }
+      peopleCount.innerHTML = newPeopleCount;
+    }
+
+    if (!isItColor[i]) {
+      this.src = "img/redheart.png";
+      isItColor[i] = true;
+    } else {
+      this.src = "img/heart.png";
+      isItColor[i] = false;
+    }
+  });
+}
 
 // normalheartBtn.addEventListener("click", function () {
 //   heartBtn.src = "img/heart.png";
