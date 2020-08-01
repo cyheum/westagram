fd809e46900498e9d1cb65b82932c7bc293ea7ba
YEHEUM\qkswk <cyheum@naver.com>
2020-08-02 03:17:12 +0900

    css, js 수정

--- js/main.js
+++ js/main.js
@@ -10,26 +10,34 @@ sectionList.forEach((section) => {
   const commentSubmitButton = section.getElementsByClassName(
     "commentButton"
   )[0];
-  const commentList = section.getElementsByTagName("ul")[0];
-
+  const commentListContainer = section.getElementsByTagName("ul")[0];
   const submitComment = () => {
     if (commentInput.value === "") {
       return;
     }
 
-    const wrapCommentLi = document.createElement("li");
-    wrapCommentLi.innerHTML = `<a>${name.text}</a>${commentInput.value}<span class="commentIcons"><img class="commentHeart colorHeart" src="img/heart.png" alt="heart" /><span class="searchXBtn"></span></span>`;
-    commentList.append(wrapCommentLi);
+    const newComment = document.createElement("li");
+    newComment.innerHTML = `<a>${name.text}</a>${commentInput.value}<span class="commentIcons"><img class="commentHeart colorHeart" src="img/heart.png" alt="heart" /><span class="searchXBtn"></span></span>`;
+    commentListContainer.append(newComment);
     commentInput.value = "";
 
-    // 새로운 댓글 하트 버튼 활성화 구현
+    // 새로운 댓글 하트 버튼, 댓글 삭제 버튼 활성화 구현
 
-    const lastHeart = wrapCommentLi.getElementsByClassName("commentHeart")[0];
-
+    const newCommentHeart = newComment.getElementsByClassName(
+      "commentHeart"
+    )[0]; //위에 생성한 li안의 className
     let isLiked = false;
-    lastHeart.addEventListener("click", function () {
+    newCommentHeart.addEventListener("click", function () {
       isLiked = !isLiked;
-      updateHeartIcon(isLiked, lastHeart); //하트 컬러 부여 함수 호출
+      updateHeartIcon(isLiked, newCommentHeart); //하트 컬러 부여 함수 호출
+    });
+
+    const newCommentRemoveBtn = newComment.getElementsByClassName(
+      "searchXBtn"
+    )[0];
+
+    newCommentRemoveBtn.addEventListener("click", function () {
+      newComment.remove();
     });
   };
 
@@ -46,7 +54,7 @@ sectionList.forEach((section) => {
     }
   });
 
-  //기존 좋아요 버튼 구현 및 좋아요 숫자 추가, 감소 기능 구현
+  //기존 좋아요 버튼, 댓글 삭제 버튼 구현 및 좋아요 숫자 추가, 감소 기능 구현
   const heartBtnList = Array.from(section.getElementsByClassName("colorHeart"));
   let peopleCount = section.getElementsByClassName("peopleCount")[0];
 
@@ -63,22 +71,108 @@ sectionList.forEach((section) => {
       }
     });
   });
+
+  const commentDOMList = Array.from(
+    commentListContainer.getElementsByTagName("li")
+  );
+  commentDOMList.forEach((comment) => {
+    const removeCommentBtn = comment.getElementsByClassName("searchXBtn")[0];
+    removeCommentBtn.addEventListener("click", function () {
+      comment.remove();
+    });
+  });
 });
 
 // 하트 컬러 부여 함수 선언
 function updateHeartIcon(isLiked, heartIcon) {
   heartIcon.src = isLiked ? "img/redheart.png" : "img/heart.png";
 }
-//댓글 3개이상이면 더보기 버튼 생성 및 높이 고정
+
+//댓글 3개이상이면 더보기 버튼 생성 및 높이 고정 (미구현)
 //const comment = document.getElementsByTagName("ul"); 위에 선언
+// const commentList = document.getElementsByTagName;
 
-const commentList = document.getElementsByTagName;
+//프로필 틀 만드는 함수 구현
+const createProfileDOM = (profileImageSrc, iD, description) => {
+  const profileDOM = document.createElement("div");
+  profileDOM.className = "wrapProfile";
+  profileDOM.innerHTML = `<img src="${profileImageSrc}" alt="profileImg" /><div><a>${iD}</a><p>${description}</p></div>`;
+  return profileDOM;
+};
 
-// nav 검색 관련
+//스토리 프로필 리스트 생성
+const storyProfileList = [
+  {
+    profileImageSrc: "img/yeonuk.png",
+    iD: "yeonukhwang",
+    lastLoginTime: "1분전",
+  },
+  {
+    profileImageSrc: "img/sanghun.png",
+    iD: "sanghun",
+    lastLoginTime: "28분전",
+  },
+  {
+    profileImageSrc: "img/dlwlrma.png",
+    iD: "dlwlrma",
+    lastLoginTime: "6시간전",
+  },
+  {
+    profileImageSrc: "img/bsjun.png",
+    iD: "bn_sj2013",
+    lastLoginTime: "7시간전",
+  },
+];
+
+// 추천프로필 리스트 생성
+const recommendProfileList = [
+  { profileImageSrc: "img/gnlcks.png", iD: "you_hchan", name: "유휘찬" },
+  { profileImageSrc: "img/ckdtlr.png", iD: "rnjsckdtlr_dd", name: "권창식" },
+  { profileImageSrc: "img/tkdrn.png", iD: "ohsan9_539", name: "오상구" },
+  { profileImageSrc: "img/profile.png", iD: "ma_sang_1", name: "마상원" },
+];
+
+//스토리 프로필, 추천 프로필 map, forEach 사용해서 함수 호출하여 생성
+
+const storyRecomProfileDOMList = document.getElementsByClassName(
+  "storyRecomWrap"
+);
+
+storyProfileList
+  .map((profileInform) =>
+    createProfileDOM(
+      profileInform["profileImageSrc"],
+      profileInform["iD"],
+      profileInform["lastLoginTime"]
+    )
+  )
+  .forEach((dom) => {
+    storyRecomProfileDOMList[0].append(dom);
+  });
+
+recommendProfileList
+  .map((profileInform) =>
+    createProfileDOM(
+      profileInform["profileImageSrc"],
+      profileInform["iD"],
+      profileInform["name"]
+    )
+  )
+  .forEach((dom) => {
+    storyRecomProfileDOMList[1].append(dom);
+  });
+
+// nav 검색 관련(focus, focusout 이벤트 구현)
+// 검색창 텍스트 지우기 버튼 구현
 const searchIcon = document.getElementsByClassName("searchIconBox")[0];
 const navText = document.getElementsByClassName("navTextBar")[0];
 const WrapNavText = document.getElementsByClassName("navTextBarCon")[0];
-const searchIconText = document.getElementsByClassName("searchText")[0];
+const searchIconTextContainer = document.getElementsByClassName(
+  "searchText"
+)[0];
+
+WrapNavText.addEventListener("mouseenter", (e) => (isCursorEntered = true));
+WrapNavText.addEventListener("mouseleave", (e) => (isCursorEntered = false));
 
 searchIcon.addEventListener("click", () => {
   WrapNavText.setAttribute("style", "z-index: 10;");
@@ -91,30 +185,40 @@ searchIcon.addEventListener("click", () => {
 navText.addEventListener("focusout", () => {
   WrapNavText.setAttribute("style", "z-index: 0;");
   if (navText.value === "") {
-    searchIconText.innerHTML = `검색`;
+    searchIconTextContainer.innerHTML = `검색`;
   } else {
-    searchIconText.innerHTML = navText.value;
+    searchIconTextContainer.innerHTML = navText.value;
   }
 });
 
+// 검색창 텍스트 지우기 버튼 구현
+const searchTextRemoveBtn = WrapNavText.getElementsByClassName("searchXBtn")[0];
+
+searchTextRemoveBtn.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+});
+
+searchTextRemoveBtn.addEventListener("click", () => {
+  navText.value = "";
+});
+
 // aside bar story 모두보기 구현
-let isStoryBtnPush = false;
+let isStoryBtnPush = [false, false];
 const storyAllBtn = document.getElementsByClassName("storyAll");
 const story = document.getElementsByClassName("storyRecomWrap");
 
 for (let i = 0; i < storyAllBtn.length; i++) {
   storyAllBtn[i].addEventListener("click", () => {
-    ClickStoryAll(i);
+    isStoryBtnPush[i] = !isStoryBtnPush[i];
+    ClickStoryAll(i, isStoryBtnPush[i]);
   });
 }
 
-const ClickStoryAll = (num) => {
-  if (!isStoryBtnPush) {
+const ClickStoryAll = (num, isStoryBtnPush) => {
+  if (isStoryBtnPush) {
     story[num].setAttribute("style", "height: auto;");
-    isStoryBtnPush = true;
   } else {
     story[num].setAttribute("style", "height: 225px");
-    isStoryBtnPush = false;
   }
 };
 
